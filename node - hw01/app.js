0e4ea21f105edbf07ccc973844624550b8a0ab11
andrey-kupets <andrey.kupets@i.ua>
2021-02-22 19:42:36 +0300

    cleaned up

--- node - hw01/app.js
+++ node - hw01/app.js
@@ -5,39 +5,31 @@ const dir1800 = path.join(__dirname, '18-00');
 const dir2000 = path.join(__dirname, '20-00');
 
 fs.readdir(dir1800, (err, files) => {
-    if (err) {
-        console.log(err);
-    }
+    if (err) console.log(err);
     files.forEach(file => {
-        console.log(file);
         fs.readFile(path.join(dir1800, file), (err1, data) => {
-            if (JSON.parse(data).gender === 'male') {
-                fs.rename(path.join(dir1800, file), path.join(dir2000, file), err1 => {
-                    if (err1) {
-                        console.log(err1)
-                    }
-                })
+            if (err1) console.log(err1);
+            if (JSON.parse(data.toString()).gender === 'male') {
+                fs.rename(path.join(dir1800, file), path.join(dir2000, file), err2 => {
+                    if (err2) console.log(err2);
+                });
             }
-        })
-    })
-})
+        });
+    });
+});
 
 fs.readdir(dir2000, (err, files) => {
-    if (err) {
-        console.log(err);
-    }
+    if (err) console.log(err);
     files.forEach(file => {
-        console.log(file);
         fs.readFile(path.join(dir2000, file), (err1, data) => {
-            if (JSON.parse(data).gender === 'female') {
-                fs.rename(path.join(dir2000, file), path.join(dir1800, file), err1 => {
-                    if (err1) {
-                        console.log(err1)
-                    }
-                })
+            if (err1) console.log(err1);
+            if (JSON.parse(data.toString()).gender === 'female') {
+                fs.rename(path.join(dir2000, file), path.join(dir1800, file), err2 => {
+                    if (err2) console.log(err2);
+                });
             }
-        })
-    })
-})
+        });
+    });
+});
 
 
