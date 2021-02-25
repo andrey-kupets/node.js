e69e2c930c46b12e3e916dc05a669e59825b8325
andrey-kupets <andrey.kupets@i.ua>
2021-02-25 23:00:58 +0300

    add linter

--- lesson02/app.js
+++ lesson02/app.js
@@ -6,32 +6,32 @@ const app = express();
 
 app.listen(5000, () => {
     console.log('app rules');
-})
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs'); // какой движок использовать для отображения
 app.engine('.hbs', expressHbs({
-    defaultLayout: false //this parameter is strictly required - способ действия, какие настройки использовать, при обработке файлов .hbs
+    // eslint-disable-next-line max-len
+    defaultLayout: false // this parameter is strictly required - способ действия, какие настройки использовать, при обработке файлов .hbs
 }));
 app.set('views', path.join((__dirname, 'static'))); // путь, где вьюшки лежат
 
-
-let users = [
-    {name: 'vasya', gender: 'male', age: 21},
-    {name: 'manya', gender: 'female', age: 18},
-    {name: 'kolya', gender: 'male', age: 27},
-    {name: 'sonya', gender: 'female', age: 17},
-    {name: 'marina', gender: 'female', age: 35},
-    {name: 'petya', gender: 'male', age: 46},
-    {name: 'ostap', gender: 'male', age: 19},
-    {name: 'violeta', gender: 'female', age: 20},
+const users = [
+    { name: 'vasya', gender: 'male', age: 21 },
+    { name: 'manya', gender: 'female', age: 18 },
+    { name: 'kolya', gender: 'male', age: 27 },
+    { name: 'sonya', gender: 'female', age: 17 },
+    { name: 'marina', gender: 'female', age: 35 },
+    { name: 'petya', gender: 'male', age: 46 },
+    { name: 'ostap', gender: 'male', age: 19 },
+    { name: 'violeta', gender: 'female', age: 20 },
 ];
 
 app.get('/login', (req, res) => {
-    res.render('login', {var: true, name: 'Serg'});
+    res.render('login', { var: true, name: 'Serg' });
 });
 
 app.post('/login', (req, res) => {
@@ -41,26 +41,25 @@ app.post('/login', (req, res) => {
     users.push(req.body);
     // res.json("User's registered now");
     res.redirect('/users');
-})
+});
 
 app.get('/users', ((req, res) => {
-    const {gender} = req.query;
+    const { gender } = req.query;
     let usersToShow = JSON.parse(JSON.stringify(users));
 
     if (gender) {
-        usersToShow = users.filter(user => user.gender === gender);
+        usersToShow = users.filter((user) => user.gender === gender);
     }
 
-    res.render('users', {users: usersToShow});
-}))
+    res.render('users', { users: usersToShow });
+}));
 
 app.get('/users/:userId', ((req, res) => {
     console.log(req.params);
-    const {userId} = req.params;
+    const { userId } = req.params;
 
-    res.json(users[userId])
-}) )
-
+    res.json(users[userId]);
+}));
 
 // app.get('/life', (req, res) => {
 //     //res.send('justice');
