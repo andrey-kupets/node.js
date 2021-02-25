e5f0841aafa8f84457cd1a08a7f1b1e9e4d1febb
andrey-kupets <andrey.kupets@i.ua>
2021-02-25 22:58:56 +0300

    add linter

--- lesson01/dir/file.js
+++ lesson01/dir/file.js
@@ -1,11 +1,12 @@
-const var22 = 22;
+// const var22 = 22;
 
 function createUser(name, age) {
+    // eslint-disable-next-line no-use-before-define
     logger(`Hello, ma name is ${name} and i'm ${age} y.o.`);
 }
 
 function logger(string) {
-    console.log(string)
+    console.log(string);
 }
 
 // console.log(55555555555)
@@ -19,4 +20,4 @@ module.exports = {
     //     console.log('tttt');
     // })(),
     // tt: logger(777)
-}
+};
