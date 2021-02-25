9d1ca08cca439b68e4e4f63638a573c8e4a5a13b
andrey-kupets <andrey.kupets@i.ua>
2021-02-25 22:24:13 +0300

    add linter

--- hw01/app.js
+++ hw01/app.js
@@ -6,11 +6,11 @@ const dir2000 = path.join(__dirname, '20-00');
 
 fs.readdir(dir1800, (err, files) => {
     if (err) console.log(err);
-    files.forEach(file => {
+    files.forEach((file) => {
         fs.readFile(path.join(dir1800, file), (err1, data) => {
             if (err1) console.log(err1);
             if (JSON.parse(data.toString()).gender === 'male') {
-                fs.rename(path.join(dir1800, file), path.join(dir2000, file), err2 => {
+                fs.rename(path.join(dir1800, file), path.join(dir2000, file), (err2) => {
                     if (err2) console.log(err2);
                 });
             }
@@ -20,16 +20,14 @@ fs.readdir(dir1800, (err, files) => {
 
 fs.readdir(dir2000, (err, files) => {
     if (err) console.log(err);
-    files.forEach(file => {
+    files.forEach((file) => {
         fs.readFile(path.join(dir2000, file), (err1, data) => {
             if (err1) console.log(err1);
             if (JSON.parse(data.toString()).gender === 'female') {
-                fs.rename(path.join(dir2000, file), path.join(dir1800, file), err2 => {
+                fs.rename(path.join(dir2000, file), path.join(dir1800, file), (err2) => {
                     if (err2) console.log(err2);
                 });
             }
         });
     });
 });
-
-
