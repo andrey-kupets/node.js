12fd8962ec40cec1f900b64e059241258ac0b225
andrey-kupets <andrey.kupets@i.ua>
2021-03-03 15:46:13 +0300

    add some stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
     ],
     plugins: [],
     rules: {
-        'linebreak-style': 0,
         'array-bracket-newline': [
             'error',
             { minItems: 2 },
@@ -42,6 +41,7 @@ module.exports = {
             4,
             { SwitchCase: 1 },
         ],
+        'linebreak-style': 'off',
         'max-classes-per-file': 'off',
         'newline-per-chained-call': 'error',
         'new-parens': 'error',
@@ -108,6 +108,10 @@ module.exports = {
         'no-unused-vars': [
             'error',
             { varsIgnorePattern: 'ctx' },
+        ],
+        'no-use-before-define': [
+            'error',
+            { functions: false }
         ],
         'no-useless-call': 'error',
         'no-useless-concat': 'error',
