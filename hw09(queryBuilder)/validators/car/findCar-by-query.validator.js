cb4ba5cd8d41381a8d07ad05b16fcc3f614edcaf
andrey-kupets <andrey.kupets@i.ua>
2021-03-14 21:58:03 +0300

    use some fix

--- hw09(queryBuilder)/validators/car/findCar-by-query.validator.js
+++ hw09(queryBuilder)/validators/car/findCar-by-query.validator.js
@@ -4,7 +4,11 @@ module.exports = Joi.object({
     model: Joi.string().alphanum().min(2).max(30),
     edition: Joi.number(),
     power_hp: Joi.number(),
-    price: Joi.allow(),
-    category: Joi.allow(),
+    price: Joi.number(), // что я только не пробовал
+    // price: Joi.allow(), // что я только не пробовал
+    // price: Joi.object(), // что я только не пробовал
+    category: Joi.string(), // что я только не пробовал
+    // category: Joi.allow(), // что я только не пробовал
+    // category: Joi.object(), // что я только не пробовал
     color: Joi.string()
 });
