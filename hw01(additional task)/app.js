de15932b4abfc29ac2d263f88134af9ccef27ab5
andrey-kupets <andrey.kupets@i.ua>
2021-05-24 21:16:11 +0300

    reduced logs

--- hw01(additional task)/app.js
+++ hw01(additional task)/app.js
@@ -4,29 +4,23 @@ const path = require('path');
 const dirBardak = path.join(__dirname, 'bardak');
 const dirStraightened = path.join(__dirname, 'straightened');
 
-function straighten(wrongDir, rightDir) {
-    fs.readdir(wrongDir, (err, files) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        files.forEach((file) => {
-            const wrongPath = path.join(wrongDir, `${file}`);
-            fs.stat(wrongPath, ((err1, stats) => {
-                if (err1) {
-                    console.log(err1);
-                    return;
-                }
-                if (stats.isDirectory()) {
-                    straighten(wrongPath, rightDir);
-                    return;
-                }
-                fs.rename(wrongPath, path.join(rightDir, `${file}`), (err2) => {
-                    if (err2) console.log(err2);
-                });
-            }));
-        });
-    });
-}
+const straighten = (wrongDir, rightDir) => {
+  fs.readdir(wrongDir, (err, files) => {
+      if (err) console.log(err);
+      files.forEach((file) => {
+          const wrongPath = path.join(wrongDir, file);
+          fs.stat(wrongPath, (err1, stats) => {
+              if (err1) console.log(err1);
+              if (stats.isDirectory()) {
+                  straighten(wrongPath, rightDir);
+                  return;
+              }
+              fs.rename(wrongPath, path.join(rightDir, file), (err2) => {
+                  if (err2) console.log(err2);
+              });
+          });
+      });
+  });
+};
 
 straighten(dirBardak, dirStraightened);
