6bfce649338027487bf1fac1d5c33f39098390a7
andrey-kupets <andrey.kupets@i.ua>
2021-05-24 19:48:02 +0300

    add cors

--- hw09(queryBuilder)/app.js
+++ hw09(queryBuilder)/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 dotenv.config({ path: path.join(process.cwd(), '../.env') });
 // dotenv.config(); тогда нужно прописать правильно путь в терминале
@@ -14,6 +15,7 @@ const app = express();
 
 _connectDB();
 
+app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
